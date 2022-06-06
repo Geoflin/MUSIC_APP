0bf9c12d43d3411e2d39d2a32837e2909cf831fb
Guillaume Baur <guilaume.baur@gmail.com>
2022-06-06 12:00:21 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Layout from 'components/UI/Layout';
+
+import Home from "components/Home";
+import About from "components/About";
+
+export default function App(){
+  return (
+    <Router>
+     <Layout>
+      <Routes>
+       <Route path="/about" element={<About/>} />
+       <Route path="/" element={<Home/>}  />
+      </Routes>
+     </Layout>
+    </Router>
+  );
+}
